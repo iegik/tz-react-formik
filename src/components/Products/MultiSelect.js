e4ffdc365c0c2c4e05c9dd7bf077c646a3791361
Arturs Jansons <a.jansons@gmail.com>
2020-03-16 14:58:10 +0300

    Fix styles

--- src/components/Products/MultiSelect.js
+++ src/components/Products/MultiSelect.js
@@ -15,15 +15,39 @@ const MultiSelect = ({
     isMulti
     value={options ? options.filter(option => value.includes(option.value)) : []}
     onBlur={onBlur}
+    invalid={invalid}
     className={classNames({
       'is-invalid': invalid,
     })}
     styles={{
       control: (style, state) => {
-        // TODO: Find better solution
-        const borderColor = state.selectProps.className === 'is-invalid' ? 'red' : 'hsl(0,0%,80%)';
+        if (state.selectProps.invalid) {
+          state.theme.colors = {
+            ...state.theme.colors,
+            primary: "#dc3545",
+            primary50: "#FFBDAD",
+          }
 
-        return { ...style, borderColor };
+          return {
+            ...style,
+            borderColor: state.theme.colors.primary,
+            boxShadow: state.isFocused ? `0 0 0 0.2rem rgba(220,53,69,.25)` : null
+          };
+        }
+
+        state.theme.colors = {
+          ...state.theme.colors,
+          primary: "#2684FF",
+          primary50: "#B2D4FF",
+          danger: "#dc3545",
+          dangerLight: "#FFBDAD",
+        }
+
+        return {
+          ...style,
+          borderColor: state.theme.colors.neutral20,
+          boxShadow: state.isFocused ? `0 0 0 0.2rem rgba(0,123,255,.25)` : null
+        };
       },
     }}
   />
